6c9fad9a5fdbe78cf64cfad850a04a2e9c9e0950
Rebeca Spiess <spiess.rebeca@gmail.com>
2020-02-17 00:55:45 -0300

    Web part

---
+++ web/scheduler.php
@@ -0,0 +1,22 @@
+<?php
+
+class scheduler
+{
+    public $enabled;
+    public $s1;
+    public $s2;
+    public $s3;
+    public $s4;
+    public $s5;
+    public $s6;
+    public $s7;
+    public $s8;
+    public $current_time_year;
+    public $current_time_month;
+    public $current_time_day;
+    public $current_time_hour;
+    public $current_time_minute;
+    public $current_time_second; 
+    
+}
+
